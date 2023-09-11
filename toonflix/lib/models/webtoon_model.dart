34c8cb43f89cb7059ec8cab0bb741c73039f6aa5
Seon Woo Kim <sw204011@gmail.com>
2023-09-11 19:30:52 +0900

    #6.5

--- toonflix/lib/models/webtoon_model.dart
+++ toonflix/lib/models/webtoon_model.dart
@@ -1,6 +1,8 @@
 class WebtoonModel {
   final String title, thumb, id;
 
+  //Map<key, value> 형태의 json을 받아와서
+
   WebtoonModel.fromJson(Map<String, dynamic> json)
       : title = json['title'],
         thumb = json['thumb'],
--- toonflix/lib/services/api_service.dart
+++ toonflix/lib/services/api_service.dart
@@ -3,26 +3,34 @@ import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon_model.dart';
 
 class ApiService {
-  final String baseurl = "https://webtoon-crawler.nomadcoders.workers.dev/";
-  final String today = 'today';
+  static const String baseurl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = 'today';
 
-  void getTodaysToons() async {
+  static Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstance = [];
     final url = Uri.parse('$baseurl/$today');
-    final response = await http.get(url);
-    if (response.statusCode == 200) {
-      final List<dynamic> webtoons = jsonDecode(response.body);
-      for (var webtoon in webtoons) {
-        final toon = WebtoonModel.fromJson(webtoon);
-        //json을 해당 모델로 보낸다.
-        //해당 모델에서 값을 받아 처리한다.
-        print(toon.title);
+
+    try {
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        final webtoons = jsonDecode(response.body);
+        for (var webtoon in webtoons) {
+          final instance = WebtoonModel.fromJson(webtoon);
+          webtoonInstance.add(instance);
+        }
+        return webtoonInstance;
+      } else {
+        throw Exception('Failed to load data: ${response.statusCode}');
       }
-      return;
-    } else {
-      throw Error();
+    } catch (e) {
+      // Handle the error here
+      print('Error: $e');
+      rethrow; // Re-throw the error if needed for further handling
     }
   }
 }
 
-// http라는 패키지를 pub.dev에서 다운로드 받음
-// get에 있는 Future요소 
+// http 패키지를 pub.dev에서 다운로드 받음
+// get에 있는 Future 요소
